ad0c31a40ddbd23a738f458c49c71ed956d7a759
Matyáš Krejza <matyas.krejza@seznam.cz>
2023-08-16 11:35:25 +0200

    2 sizes of lineup

--- src/components/ArtistBubbles.svelte
+++ src/components/ArtistBubbles.svelte
@@ -4,7 +4,14 @@
 	import ArtistBubble from './ArtistBubble.svelte';
 
 	export let artists: Array<ArtistType>;
-	let artistStyles = [
+	export let isBig: boolean;
+	let artistBigStyles = [
+		'desktop:w-[90px] w-[60px] rounded-full ring ring-primary ring-offset-base-100 mt-1',
+		'desktop:w-[84px] w-[54px] rounded-full ring ring-primary ring-offset-base-100 mt-12',
+		'desktop:w-[92px] w-[62px] rounded-full ring ring-primary ring-offset-base-100 mt-1',
+		'desktop:w-[88px] w-[58px] rounded-full ring ring-primary ring-offset-base-100 mt-8'
+	];
+	let artistSmallStyles = [
 		'desktop:w-[60px] w-[40px] rounded-full ring ring-primary ring-offset-base-100 m-1 mt-1',
 		'desktop:w-[54px] w-[34px] rounded-full ring ring-primary ring-offset-base-100 m-1 mt-8',
 		'desktop:w-[62px] w-[42px] rounded-full ring ring-primary ring-offset-base-100 m-1 mt-2',
@@ -12,15 +19,36 @@
 	];
 </script>
 
-<div class="w-1/2 flex flex-row items-center justify-center h-24 rounded-md drop-shadow-lg ml-1">
-	<div class="stack w-full h-full items-center justify-center mt-2">
-		<button class="btn btn-primary desktop:w-20 w-16">Lineup</button>
-		<div class="flex flex-row items-start justify-center w-full h-full">
-			<ArtistBubble artist={artists[0]} artistClass={artistStyles[0]} />
-			<ArtistBubble artist={artists[1]} artistClass={artistStyles[1]} />
-			<div class="desktop:mx-2" />
-			<ArtistBubble artist={artists[2]} artistClass={artistStyles[2]} />
-			<ArtistBubble artist={artists[3]} artistClass={artistStyles[3]} />
+{#if isBig}
+	<div
+		class="w-full flex flex-row items-center justify-center desktop:h-40 h-28 rounded-md drop-shadow-lg"
+	>
+		<div class="flex relative w-full h-full items-center justify-center mt-2">
+			<div
+				class="flex flex-row items-start justify-center w-full h-full transform transition-all absolute"
+			>
+				<ArtistBubble artist={artists[0]} artistClass={artistBigStyles[0]} />
+				<ArtistBubble artist={artists[1]} artistClass={artistBigStyles[1]} />
+				<div class="desktop:mx-14 mx-6" />
+				<ArtistBubble artist={artists[2]} artistClass={artistBigStyles[2]} />
+				<ArtistBubble artist={artists[3]} artistClass={artistBigStyles[3]} />
+			</div>
+			<button class="btn btn-primary desktop:w-40 w-28 transform transition-all absolute"
+				>Lineup</button
+			>
 		</div>
 	</div>
-</div>
+{:else}
+	<div class="w-1/2 flex flex-row items-center justify-center h-24 rounded-md drop-shadow-lg">
+		<div class="stack w-full h-full items-center justify-center mt-2">
+			<button class="btn btn-primary desktop:w-20 w-16">Lineup</button>
+			<div class="flex flex-row items-start justify-center w-full h-full">
+				<ArtistBubble artist={artists[0]} artistClass={artistSmallStyles[0]} />
+				<ArtistBubble artist={artists[1]} artistClass={artistSmallStyles[1]} />
+				<div class="desktop:mx-2" />
+				<ArtistBubble artist={artists[2]} artistClass={artistSmallStyles[2]} />
+				<ArtistBubble artist={artists[3]} artistClass={artistSmallStyles[3]} />
+			</div>
+		</div>
+	</div>
+{/if}
--- src/routes/[eventId]/+page.svelte
+++ src/routes/[eventId]/+page.svelte
@@ -3,10 +3,18 @@
 	import type { PageData } from './$types';
 	import ArtistBubbles from '../../components/ArtistBubbles.svelte';
 	import EventInfoButtonSmall from '../../components/EventInfoButtonSmall.svelte';
+	import ReloadIcon from '../../components/icons/ReloadIcon.svelte';
 
 	export let data: PageData;
 
+	let lineupBig = false;
+
 	const startDate = Date.parse(data.event.start_time);
+
+	function changeLineupSize() {
+		lineupBig = !lineupBig;
+		console.log(lineupBig);
+	}
 </script>
 
 <div class="w-full h-full items-center justify-center p-6">
@@ -37,18 +45,41 @@
 				{data.event.description}
 			</h>
 		</div>
-		<div class="flex flex-row w-full items-center justify-center my-2">
-			<EventInfoButtonSmall
-				text={'Ig'}
-				icon="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph-900x900.png"
-				link={data.event.event_instagram}
-			/>
-			<EventInfoButtonSmall
-				text={'Buy tix!! :3'}
-				icon="https://icon-library.com/images/ticket-icon-png/ticket-icon-png-12.jpg"
-				link={data.event.tickets_url}
-			/>
-			<ArtistBubbles artists={data.artists} />
+		<div class="w-full flex flex-col items-end mt-2">
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<ReloadIcon on:click={changeLineupSize} />
 		</div>
+		{#if lineupBig}
+			<div class="flex flex-col w-full items-center justify-center my-2">
+				<ArtistBubbles artists={data.artists} isBig={lineupBig} />
+				<div class="flex flex-row w-full items-center justify-center desktop:m-2 m-6">
+					<EventInfoButtonSmall
+						text={'Ig'}
+						icon="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph-900x900.png"
+						link={data.event.event_instagram}
+					/>
+					<EventInfoButtonSmall
+						text={'Buy tix!! :3'}
+						icon="https://icon-library.com/images/ticket-icon-png/ticket-icon-png-12.jpg"
+						link={data.event.tickets_url}
+					/>
+				</div>
+			</div>
+		{:else}
+			<div class="flex flex-row w-full items-center justify-center my-2">
+				<EventInfoButtonSmall
+					text={'Ig'}
+					icon="https://www.edigitalagency.com.au/wp-content/uploads/new-Instagram-logo-white-glyph-900x900.png"
+					link={data.event.event_instagram}
+				/>
+				<EventInfoButtonSmall
+					text={'Buy tix!! :3'}
+					icon="https://icon-library.com/images/ticket-icon-png/ticket-icon-png-12.jpg"
+					link={data.event.tickets_url}
+				/>
+				<ArtistBubbles artists={data.artists} isBig={lineupBig} />
+			</div>
+		{/if}
 	</div>
 </div>
