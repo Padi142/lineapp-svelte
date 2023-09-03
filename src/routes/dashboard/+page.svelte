d674de0d22e1b9039f26d2481bce7ef333c9e2bc
Matyáš Krejza <matyas.krejza@seznam.cz>
2023-09-03 23:47:33 +0200

    hotfix

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -46,7 +46,7 @@
 						src={event.event_logo}
 						alt={event.event_name}
 					/>
-					<h1 class="m-1 text-center">
+					<h1 class="m-1 text-center text-ellipsis overflow-y-hidden">
 						{event.event_name}
 					</h1>
 				</div>
